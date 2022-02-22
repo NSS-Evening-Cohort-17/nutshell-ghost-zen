1b2035694402dae8cc5e75e74d6158dd73e59bec
madison-hepner <hepnermadison@icloud.com>
2022-02-21 19:24:53 -0600

    basics of the task card, do not merge

---
+++ src/components/tasks/TaskCard.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./TaskCard.css"
+
+export const TaskCard = () => (
+    <section className="tasks">
+        <h3 className="task__title">(ex). Go To Grocery Stores</h3>
+        <p>Priority Status: </p>
+        <p className="task__descrip">notes of task</p>
+        <p>Due on: </p>
+    </section>
+)
