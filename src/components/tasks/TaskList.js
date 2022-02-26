b64a3281e9499e0f0fa7d5f11161cff7da9a489f
madison-hepner <hepnermadison@icloud.com>
2022-02-26 12:22:59 -0600

    tasks match styling

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { TaskCard } from './TaskCard';
 import { getAllTasks, deleteTask } from '../modules/TaskManager';
 import { useNavigate} from "react-router-dom"
+import "./../Nutshell.css"
 
 export const TaskList = () => {
   // The initial state is an empty array
@@ -30,11 +31,9 @@ export const TaskList = () => {
     <h3 className="page__title">Tasks</h3>
 
     <section className="section-content">
-        <button type="button"
-          className="big__btn"
-            onClick={() => {navigate("/tasks/create")}}>
-            add task
-          </button>
+        <div className="big__btns">
+            <button type="button" className="big__btn btn" id="big__btn" onClick={() => {navigate("/tasks/create")}} >Add a Task</button>
+        </div>
         </section>
 
     <div className="tasks__card">
