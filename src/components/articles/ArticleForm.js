d0a8551bd346879bec936e3d7fd09d99c0a02902
brianminges <btminges@gmail.com>
2022-02-25 20:05:38 -0600

    Articles done with CSS

--- src/components/articles/ArticleForm.js
+++ src/components/articles/ArticleForm.js
@@ -1,7 +1,10 @@
 import react, { useState } from "react";
 import { useNavigate } from "react-router-dom"
 import { addArticle } from './../modules/ArticlesManager'
+import './ArticleForm.css'
+import './../Nutshell.css'
 
+//This is the form users use to add new articles. Coded by Brian. 
 
 export const AddArticleForm = () => {
     const sessionUser = JSON.parse(window.sessionStorage.getItem("nutshell_user"))
@@ -26,7 +29,7 @@ export const AddArticleForm = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        if (article.url === null || article.title === null || article.synopsis === null) {
+        if (article.url === "" || article.title === "" || article.synopsis === "") {
             window.alert('All fields must be filled in')
         } else {
             addArticle(article)
@@ -36,30 +39,35 @@ export const AddArticleForm = () => {
 
     return (
         <form>
-            <h2 className="page__title">Add an article</h2>
-            <fieldset>
-                <div>
-                    <label htmlFor="title">Headline</label>
-                    <input type="text" id="title" onChange={handleInputChange} required value={article.title} /> 
+            <div  className="form__inputs">
+                <h2 className="page__title">Add an article</h2>
+                <fieldset>
+                    <div  className="form__input">
+                        <label htmlFor="title" className="form__input__label" >Headline</label>
+                        <input type="text" className="form__input__field" id="title" onChange={handleInputChange} required value={article.title} /> 
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <div  className="form__input">
+                        <label htmlFor="synopsis" className="form__input__label" >Synopsis</label>
+                        <input type="text" className="form__input__field" id="synopsis" onChange={handleInputChange} required value={article.synopsis} /> 
+                        
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <div  className="form__input">
+                        <label htmlFor="url" className="form__input__label">URL</label>
+                        <input type="url" className="form__input__field" id="url" onChange={handleInputChange} required value={article.url} /> 
+                    </div>
+                </fieldset>
+                <div className="form__input crud__btn">
+                    <button className="submit__btn"
+                        onClick={handleSubmit}>
+                        Submit
+                    </button>
                 </div>
-            </fieldset>
-            <fieldset>
-                <div>
-                    <label htmlFor="synopsis">Synopsis</label>
-                    <input type="text" id="synopsis" onChange={handleInputChange} required value={article.synopsis} /> 
-                </div>
-            </fieldset>
-            <fieldset>
-                <div>
-                    <label htmlFor="url">URL</label>
-                    <input type="url" id="url" onChange={handleInputChange} required value={article.url} /> 
-                </div>
-            </fieldset>
-
-            <button className="crud__btn btn"
-                onClick={handleSubmit}>
-                Submit
-            </button>
+                
+            </div>
         </form>
     )
 }
